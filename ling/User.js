db0adb115415d3fc79490f1340b2864c74142821
Luk Lu <luk.lu@faronear.org>
2019-12-06 11:09:12 +0800

    u

--- ling/User.js
+++ ling/User.js
@@ -4,6 +4,7 @@ const Uuid = require('uuid')
 const ticCrypto = require('tic.crypto')
 const Messenger = require('so.base/Messenger.js')
 const Webtoken = require('so.base/Webtoken.js')
+const Internation = require('../../../so/so.base/Internation.js')
 
 /****************** 类和原型 *****************/
 const DAD = module.exports = function User (prop) { // 构建类
@@ -34,72 +35,99 @@ const my={}
 DAD.api=DAD.api1={}
 
 DAD.api.identify = DAD.api1.identify = async function(option){
-  let identifyState, uuid
-  if (option.User && /^(\+\d{1,3}-)(\d{7,11})$/.test(option.User.phone)) {
+  if (option.User && Internation.validatePhone({phone:option.User.phone})) {
     let user = await DAD.getOne({User: {phone:option.User.phone}})
+    let _state, uuid
     if (user) {
       uuid = user.uuid
-      identifyState = 'OLD_USER'
+      _state = 'OLD_USER'
     } else {
       uuid = Uuid.v4(),
-      identifyState = 'NEW_USER'
+      _state = 'NEW_USER'
     }
-  }else {
-    identifyState = 'INPUT_BAD_FORMAT'
-  }
-  return { 
-    identifyState,
-    uuid,
-    _passtoken: Webtoken.createToken({
-      phone: option.User.phone,
+    return {
+      _state,
       uuid,
-      identifyState
-    })
+      _passtoken: Webtoken.createToken({
+        phone: option.User.phone,
+        uuid,
+        identifyState: _state
+      })
+    }
   }
+  return { _state: 'INPUT_MALFORMED' }
 }
 
 DAD.api.sendPasscode = async function(option){
+  let _state
   let passcode = ticCrypto.randomNumber({length:6})
+  console.log('passcode = '+passcode)
   let passcodeHash = ticCrypto.hash(passcode+option._passtokenSource.uuid)
-  let passcodeState = undefined
+  console.log('uuid = '+option._passtokenSource.uuid)
+  console.log('passcodeHash = '+passcodeHash)
+  console.log('phone = '+option._passtokenSource.phone)
+  let passcodeSentAt = undefined
   let passcodeExpireAt = undefined
   // send SMS
-  let sendResult = await Messenger.sendSms(
+  let sendResult = {state:'DONE'} /*await Messenger.sendSms(
     option._passtokenSource.phone, 
     { vendor: 'aliyun',
-      msgParam: passcode,
+      msgParam: {code: passcode},
       templateCode: 'SMS_142465215',
       signName: 'LOG'
     }
-  )
+  )*/
   if (sendResult.state==='DONE') {
-    passcodeState = 'PASSCODE_SENT'
-  }else{
-    passcodeState = 'PASSCODE_UNSENT'
+    _state = 'PASSCODE_SENT'
+    passcodeSentAt = new Date()
     passcodeExpireAt = new Date(Date.now()+5*60*1000)
-  }
-  return {
-    passcodeState,
-    passcodeHash,
-    passcodeExpireAt,
-    _passtoken: Webtoken.createToken(Object.assign(
-      option._passtokenSource, 
-      {
-        passcode,
-        passcodeState,
-        passcodeExpireAt
-      })
-    )
+    return {
+      _state,
+      passcodeHash,
+      passcodeSentAt,
+      passcodeExpireAt,
+      _passtoken: Webtoken.createToken(Object.assign(
+        option._passtokenSource, { 
+          passcodeHash,
+          passcodeState: _state,
+          passcodeSentAt,
+          passcodeExpireAt,
+        }
+      ))
+    }
+  }else{
+    return {
+      _state: 'PASSCODE_UNSENT'
+    }
   }
 }
 
 DAD.api.verifyPasscode = async function(option){
-
+  let _state
+  if (option && option.User.passcode && option._passtokenSource && new Date() < new Date(option._passtokenSource.passcodeExpireAt)) {
+    if (ticCrypto.hash(option.User.passcode+option._passtokenSource.uuid)===option._passtokenSource.passcodeHash) {
+      _state = 'VERIFY_SUCCESS'
+    }else{
+      _state = 'VERIFY_FAILED'
+    }
+    return { 
+      _state,
+      _passtoken: Webtoken.createToken(Object.assign(
+        option._passtokenSource, {
+          verifyState: _state
+        }
+      ))
+    }
+  }
+  return {
+    _state: 'INPUT_MALFORMED'
+  }
 }
 
 DAD.api.register = DAD.api1.register = async function(option){
   if (option.User && option.User.phone && option.User.passwordClient
-    && option._passtokenSource && option._passtokenSource.identifyState === 'NEWUSER'
+    && option._passtokenSource && option._passtokenSource.identifyState === 'NEW_USER'
+    && option._passtokenSource.verifyState === 'VERIFY_SUCCESS'
     && option.User.phone === option._passtokenSource.phone) {
       option.User.passwordServer = ticCrypto.hash(option.User.passwordClient + option._passtokenSource.uuid)
       option.User.uuid = option._passtokenSource.uuid
@@ -107,14 +135,14 @@ DAD.api.register = DAD.api1.register = async function(option){
       if (user) {
         option._passtokenSource.state = 'ONLINE'
         return { 
-          registerState = 'REGISTER_SUCCESS',
+          registerState: 'REGISTER_SUCCESS',
           User: user,
           _passtoken: Webtoken.createToken(option._passtokenSource) }
       }else {
         return { registerState: 'REGISTER_FAILED' }
       }
   }
-  return { registerState: 'INPUT_BAD_FORMAT' }
+  return { registerState: 'INPUT_MALFORMED' }
 }
 
 DAD.api.login = DAD.api1.login = async function(){
