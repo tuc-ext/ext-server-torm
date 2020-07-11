ab3d7b08ddd763945f907f6b9eb47aa88ee806a3
Luk Lu <luk.lu@faronear.org>
2020-07-11 16:47:35 +0800

    u

--- ConfigDynamic.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-module.exports = {
-  weixingroup: 'https://weixin.qq.com/g/AQYAANMGzp5iwkbmHwZ7SnNVLVqJ9QG4jVjdpKy_kmIoQh3Dgaw_lwE94kAlbuKK',
-  // https://www.boc.cn/sourcedb/whpj/ 中国银行外汇牌价
-  usd2cny: 699.43 
-}
