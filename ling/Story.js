a29ed582f11c2f0579a0af20fa98b16531d9c37c
Luk Lu <luk.lu@faronear.org>
2020-07-27 09:36:17 +0800

    支持前端修改已存在的故事

--- ling/Story.js
+++ ling/Story.js
@@ -26,18 +26,21 @@ DAD.api.getStoryList = async ({ placeUuid } = {}) => {
 
 DAD.api.publish = async ({ _passtokenSource, story: { author, placeUuid, storyContent, uuid } = {} }) => {
   if (_passtokenSource && author && _passtokenSource.uuid === author.uuid) {
+    let nowTimeUnix = Date.now()
     if (uuid) {
-      let story = await DAD.findOne({ uuid: uuid, placeUuid: placeUuid })
+      let story = await DAD.findOne({ uuid: uuid })
       if (story && story.author && story.author.uuid === _passtokenSource.uuid && story.placeUuid === placeUuid) {
-        await DAD.update({ uuid: uuid }, { placeUuid, author, storyContent, editTimeUnix: Date.now() })
-        return { _state: 'SUCCESS', story: { uuid: uuid } }
+        await DAD.update({ uuid: uuid }, { storyContent, editTimeUnix: nowTimeUnix })
+        story.storyContent = storyContent
+        story.editTimeUnix = nowTimeUnix
+        return { _state: 'SUCCESS', story }
       } else {
         return { _state: 'UNMATCHED_STORY' }
       }
     } else {
-      let createTimeUnix = Date.now()
-      let story = await DAD.save({ placeUuid, author, storyContent, createTimeUnix, editTimeUnix: createTimeUnix })
-      return { _state: 'SUCCESS', story: { uuid: story.uuid } }
+      let story = await DAD.save({ placeUuid, author, storyContent, createTimeUnix: nowTimeUnix, editTimeUnix: nowTimeUnix })
+      story = await DAD.findOne({ uuid: story.uuid })
+      return { _state: 'SUCCESS', story }
     }
   }
   return { _state: 'UNAUTHORIZED_AUTHOR' }
