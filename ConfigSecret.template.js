24d4adca2a35fa058ca6eb51a6a8a75714c9b3b0
luk.lu <luk.lu@faronear.org>
2021-07-06 13:17:00 +0800

    u

---
+++ ConfigSecret.template.js
@@ -0,0 +1,19 @@
+/****************************************************************
+ *  ！！！机密配置！！！
+ * 只能在部署时出现，严禁存入GIT库，严禁在网络上传送，严禁公开！
+**/
+
+module.exports={
+  tokenKey: '？？？？p[aerjklasdf9q34@#$-SCDAW',
+  secword: '？？？？十二字的助记词',
+  SMS: {
+    dxton:{
+      urlChina:'http://sms.106jiekou.com/utf8/sms.aspx?account=？？？？&password=？？？？',
+      urlWorld:'http://sms.106jiekou.com/utf8/worldapi.aspx?account=？？？？&password=？？？？',
+    },
+    aliyun:{
+      accessKeyId:'？？？？',
+      secretAccessKey:'？？？？',
+    }
+  },
+}
