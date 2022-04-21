4bd4810c0604bb56b94911b87c4040d58ae8ce91
Luk Lu <luk.lu@faronear.org>
2022-04-21 20:13:25 +0800

    change default port base from 60000 to 6000

--- ConfigBasic.js
+++ ConfigBasic.js
@@ -16,7 +16,7 @@ module.exports = {
 
   Base_Protocol: 'http', // http|https|httpall
   Base_Hostname: 'localhost', // 本节点的从外部可访问的 IP or Hostname，用于告知邻居节点怎样连接本机。因此不能是 127.0.0.1 或 localhost
-  Base_Port: 60000 + parseInt(coretool.name2port(Sys_Code_Name)), // 本节点的 Web服务端口号
+  Base_Port: 6000 + parseInt(coretool.name2port(Sys_Code_Name)), // 本节点的 Web服务端口号
   // 数据库设置
   Data_Store: { type: 'sqlite', database: `_datastore/db.sqlite` },
   // Log_Store: { type: 'file', root: '_logstore', file: 'ext-server.log' }, // 换用 pm2 的日志记录
